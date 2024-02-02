816b83ec7f73630c8f345c79cd334c371964383a
liana <liana300291@gmail.com>
2024-02-02 17:20:29 +0300

    added Layout for frontend

---
+++ client/src/components/Deposits.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+
+export default function Deposits() {
+  //const algorithms = ['Holt', 'ARIMA', 'Kalman', 'SPS'];
+
+  return (
+    <React.Fragment>
+      <div>
+      </div>
+    </React.Fragment>
+  );
+}
