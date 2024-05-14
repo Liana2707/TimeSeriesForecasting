Added Algorithms: Sign-Perturbed Sums and ARIMA modified kalman_filter, LinearRegression, holt and window_slider added correct zoom for x-axis

server/algorithms/LinearRegression.py
@@ -8,6 +8,7 @@ from algorithms.base_algorithm import BaseAlgorithm
 from algorithms.window_slider import WindowSlider
 from algorithms import UPLOAD_FOLDER
 from uploading_data.uploader import Uploader
+import statsmodels.api as sm
 
 
 
@@ -15,6 +16,10 @@ class LinearRegressionAlgorithm(BaseAlgorithm):
     def predict(self, file_name): 
         slider = WindowSlider(file_name, int(self.params['window_size']), self.date_column, self.value_column)
         self.trends = []
+        self.obs_ci_lower, self.obs_ci_upper = [], []
+        self.mean_ci_lower, self.mean_ci_upper = [], []
+        linear_trends  = []
+        mean_ci_lower, mean_ci_upper = [], []
         
         for df in slider.slide():
             df[self.date_column] = pd.to_datetime(df[self.date_column])
@@ -22,26 +27,39 @@ class LinearRegressionAlgorithm(BaseAlgorithm):
 
             date_index_datetime =  df.index.astype(np.int64) 
 
-            scaler = StandardScaler()
+            scaler = StandardScaler() # to datapreparer
             x = date_index_datetime.to_numpy().reshape(-1, 1)
-            x = scaler.fit_transform(x)
+            x = sm.add_constant(scaler.fit_transform(x))
             
             y = df 
-            model = LinearRegression()
-            model.fit(x, y)
+            model = sm.OLS(y, x).fit()
 
-            # y = k*x + b
+            results = model.get_prediction()
+            predictions_summary_frame = results.summary_frame()
 
-            model_linear_y = model.predict(x)
+            confidence_intervals = predictions_summary_frame[['mean_ci_lower', 'mean_ci_upper']]
 
-            points_list = [] 
-            for i in range(0, len(model_linear_y)):
-                point = {'x': str(df.index[i]- pd.Timestamp("1970-01-01")) // pd.Timedelta('1ms'),
-                         'y': model_linear_y[i][0]}
-                points_list.append(point)
+            chunk_prediction = model.predict(x)
 
-            self.trends.append(points_list)
 
-        return self.trends, []
+            
+            trend_point = {'x': str(df.index[len(chunk_prediction) - 1]- pd.Timestamp("1970-01-01")) // pd.Timedelta('1ms'),
+                        'y': chunk_prediction[len(chunk_prediction) - 1]}
+            
+            mean_ci_lower_point = {'x': str(df.index[len(chunk_prediction) - 1]- pd.Timestamp("1970-01-01")) // pd.Timedelta('1ms'),
+                        'y': confidence_intervals['mean_ci_lower'][len(chunk_prediction) - 1]}
+            mean_ci_upper_point = {'x': str(df.index[len(chunk_prediction) - 1]- pd.Timestamp("1970-01-01")) // pd.Timedelta('1ms'),
+                        'y': confidence_intervals['mean_ci_upper'][len(chunk_prediction) - 1]}
+                
+            linear_trends.append(trend_point)
+            mean_ci_lower.append(mean_ci_lower_point)
+            mean_ci_upper.append(mean_ci_upper_point)
+
+
+        self.trends.append(linear_trends)
+        self.mean_ci_lower.append(mean_ci_lower)
+        self.mean_ci_upper.append(mean_ci_upper)  
+            
+        return self.trends, self.obs_ci_lower, self.obs_ci_upper, self.mean_ci_lower, self.mean_ci_upper
     
         

server/algorithms/SPS.py
@@ -1,6 +1,137 @@
+import numpy as np
+import pandas as pd
+import statsmodels.api as sm
+from matplotlib import pyplot as plt
+from sklearn.discriminant_analysis import StandardScaler
 from algorithms.base_algorithm import BaseAlgorithm
+from algorithms.window_slider import WindowSlider
 
 
 class SPSAlgorithm(BaseAlgorithm):
-    def predict(self, data):
-        pass
+
+    def initialize(self):
+        self.q = float(self.params['q'])
+        self.m = int(self.params['m'])
+        self.N = int(self.params['window_size'])
+
+
+        self.signs = np.random.randint(0, 2, size=(self.N, self.m - 1)) * 2 - 1
+        self.signs = np.hstack((np.ones((self.N, 1)), self.signs))
+
+        self.tie_order = np.random.permutation(self.m)
+
+    def determine_rank(self, functions):
+        # сортировка по убыванию функций
+        sorted_functions, sorted_indexes = sorted(functions, reverse=True), sorted(range(len(functions)), key=lambda i: functions[i], reverse=True)
+
+        # Поиск позиций тех функций, которые равны по значению с delta-суммой(ссылочной суммой, H0)
+        tie_indexes = [i for i, val in enumerate(sorted_functions) if val == functions[0]]
+
+        # Индексы, которые находятся на этих позициях
+        indexes_to_sort = [sorted_indexes[i] for i in tie_indexes]
+
+        #сортировка в соответствии с перестановкой
+        idx_order = sorted(indexes_to_sort, key=lambda i: self.tie_order[i], reverse=True)
+
+        for i, val in zip(tie_indexes, idx_order):
+            sorted_indexes[i] = val
+
+        rang = sorted_indexes.index(0)
+        return rang
+
+
+    def is_parameter_in_confidence_interval(self, theta, Y, X):
+        # Y = X * theta + E
+        theta = theta.reshape(-1, 1)
+
+        # Создаём матрицу, из которой потом будем считать sign portubated sums
+        matrix = np.zeros((len(Y), self.m))
+
+        # В качестве первого столбца просто элементы delta = Y - theta X
+        delta = np.array(Y - (theta.T @ X.T)[0])
+        for i in range(len(delta)):
+            matrix[i, 0] = delta[i]
+
+        # Заполнение остальных столбцов матрицы
+        for k in range(1, self.m):
+            matrix[:, k] = matrix[:, 0] * self.signs[:, k]
+
+        # Расчет значения сумм
+        sign_perturbed_sums = np.zeros(self.m)
+        for k in range(self.m):
+            sum = np.sum(matrix[:, k])
+            sign_perturbed_sums[k] = sum**2
+
+        rang = self.determine_rank(sign_perturbed_sums)
+
+        in_confidence_set = rang >= self.q
+
+        return in_confidence_set
+
+    def predict(self, file_name):
+        self.initialize()
+        slider = WindowSlider(file_name, int(self.params['window_size']), self.date_column, self.value_column)
+        self.trends = []
+        self.obs_ci_lower, self.obs_ci_upper = [], []
+        self.mean_ci_lower, self.mean_ci_upper = [], []
+        linear_trends  = []
+        mean_ci_lower, mean_ci_upper = [], []
+        for df in slider.slide():
+            df[self.date_column] = pd.to_datetime(df[self.date_column])
+            df.set_index(self.date_column, inplace=True)
+
+            date_index_datetime =  df.index.astype(np.int64) 
+
+            scaler = StandardScaler() # to datapreparer
+            x = date_index_datetime.to_numpy().reshape(-1, 1)
+            x = scaler.fit_transform(x)
+            x_ = sm.add_constant(x)
+            y = df[self.value_column].to_numpy()
+
+            x0 = x_[0][1]
+            y0 = y[0]
+
+            k = np.arange(-10, 10, 0.05)
+            b = []
+            for i in range(len(k)):
+                current_b = y0 - k[i] * x0
+                b.append(current_b)
+            b = np.array(b)
+            lines_coeff = []
+            for i in range(len(k)):
+                lines_coeff.append([b[i], k[i]])
+
+            min_k, max_k, min_b, max_b = np.max(k), np.min(k), np.max(b), np.min(b)
+
+
+            for line in lines_coeff:
+                params = np.array(line)
+                in_conf = self.is_parameter_in_confidence_interval(params, y, x_)
+                
+                if in_conf:
+                    if min_k >= line[1]:
+                        min_k = line[1]
+                        min_b = line[0]
+                    if max_k <= line[1]:
+                        max_k = line[1]
+                        max_b = line[0]
+
+            trend_point = {'x': str(df.index[len(y) - 1]- pd.Timestamp("1970-01-01")) // pd.Timedelta('1ms'),
+                        'y': str(((min_k+(max_k-min_k)/2.0)*x[-1][0] + min_b+(max_b-min_b)/2.0))}
+            mean_ci_lower_point = {'x': str(df.index[len(y) - 1]- pd.Timestamp("1970-01-01")) // pd.Timedelta('1ms'),
+                        'y': str(min_k*x[-1][0] + min_b)}
+            mean_ci_upper_point = {'x': str(df.index[len(y) - 1]- pd.Timestamp("1970-01-01")) // pd.Timedelta('1ms'),
+                        'y': str(max_k*x[-1][0] + max_b)}
+              
+                
+            mean_ci_lower.append(mean_ci_lower_point)
+            mean_ci_upper.append(mean_ci_upper_point)
+            linear_trends.append(trend_point)
+
+
+        self.trends.append(linear_trends)
+        self.mean_ci_lower.append(mean_ci_lower)
+        self.mean_ci_upper.append(mean_ci_upper)   
+
+        return self.trends, self.obs_ci_lower, self.obs_ci_upper, self.mean_ci_lower, self.mean_ci_upper
+

server/algorithms/holt.py
@@ -1,11 +1,14 @@
 import os 
 
+import numpy as np
 import pandas as pd
 from statsmodels.tsa.api import Holt
 
 from algorithms.base_algorithm import BaseAlgorithm
 from algorithms import UPLOAD_FOLDER
+from algorithms.window_slider import WindowSlider
 from uploading_data.uploader import Uploader
+from scipy.stats import t
 
 
 class HoltAlgorithm(BaseAlgorithm):
@@ -16,41 +19,53 @@ class HoltAlgorithm(BaseAlgorithm):
         self.smoothing_trend = float(self.params['beta'])
         
     def predict(self, file_name):
-        #горизонт по идее тоже желательно с сервера получать
-        #лучше также проверить настройки
-        #отправление данных в виде двух списков тоже не нужно
 
-        file_path = os.path.join(UPLOAD_FOLDER, file_name)
-        use_cols = [self.date_column, self.value_column]
-        uploader = Uploader()
-        df = uploader.upload(path=file_path, usecols=use_cols)[0]
+        slider = WindowSlider(file_name, int(self.params['window_size']), self.date_column, self.value_column)
+        self.trends = []
+        self.obs_ci_lower, self.obs_ci_upper = [], []
+        self.mean_ci_lower, self.mean_ci_upper = [], []
+        trends = []
+        mean_ci_lower, mean_ci_upper = [], []
         
-        horizon_ = 10 
-        
-        fit = Holt(df[self.value_column], initialization_method="estimated").fit(
-        smoothing_level=self.smoothing_level, 
-        smoothing_trend=self.smoothing_trend, 
-        optimized=False
-        )
+        for df in slider.slide():
+            df[self.date_column] = pd.to_datetime(df[self.date_column])
+            df.set_index(self.date_column, inplace=True)
 
-        fcast = fit.forecast(horizon_).rename("Holt's linear trend")
+            fit = Holt(df[self.value_column], initialization_method="estimated").fit(
+            smoothing_level=self.smoothing_level, 
+            smoothing_trend=self.smoothing_trend, 
+            optimized=False
+            )
+            mean = np.mean(fit.fittedvalues)
+            std_dev = np.std(fit.fittedvalues, ddof=1) 
+            degrees_of_freedom = len(fit.fittedvalues) - 1
+            
+            t_score = t.ppf((1 + 0.95) / 2, degrees_of_freedom)
+            
+            standard_error = std_dev / np.sqrt(len(fit.fittedvalues) - 1)
+            
+            margin_of_error = t_score * standard_error
+            lower_bound = mean - margin_of_error
+            upper_bound = mean + margin_of_error
 
-        trend = fit.trend
+            trend_point = {'x': str(df.index[len(fit.fittedvalues)-1] - pd.Timestamp("1970-01-01")) // pd.Timedelta('1ms'),
+                           'y': fit.fittedvalues[-1]}
+            mean_ci_lower_point = {'x': str(df.index[len(fit.fittedvalues)-1] - pd.Timestamp("1970-01-01")) // pd.Timedelta('1ms'),
+                                   'y': str(fit.fittedvalues[-1] - (upper_bound - lower_bound)/2.0) }
+            mean_ci_upper_point = {'x': str(df.index[len(fit.fittedvalues)-1] - pd.Timestamp("1970-01-01")) // pd.Timedelta('1ms'),
+                                   'y': str(fit.fittedvalues[-1] + (upper_bound - lower_bound)/2.0)}
+            
+                                   
 
-        dates, values = [], []
-        dataset = []
+            trends.append(trend_point)
+            mean_ci_lower.append(mean_ci_lower_point)
+            mean_ci_upper.append(mean_ci_upper_point)
 
-        for i in range(0, len(df)):
-            if i == 0 or i == len(df) - 1:
-                continue
-            x_values = [df[self.date_column][i - 1], df[self.date_column][i + 1]]
-            y_values = [df[self.value_column][i] - trend[i], df[self.value_column][i] + trend[i]]
+        self.trends.append(trends)
+        self.mean_ci_lower.append(mean_ci_lower)
+        self.mean_ci_upper.append(mean_ci_upper)
+        #print(self.mean_ci_lower, self.mean_ci_upper)
 
-            dataset.append([{'x': str(x_values[0]- pd.Timestamp("1970-01-01")) // pd.Timedelta('1ms'), 
-                             'y': y_values[0]}, 
-                            {'x': str(x_values[1]- pd.Timestamp("1970-01-01")) // pd.Timedelta('1ms'), 'y': y_values[1]}
-                            ])
-
-        return dataset, []
+        return self.trends, self.obs_ci_lower, self.obs_ci_upper, self.mean_ci_lower, self.mean_ci_upper
         
         

server/algorithms/kalman_filter.py
@@ -5,9 +5,10 @@ from algorithms.base_algorithm import BaseAlgorithm
 from filterpy.kalman import KalmanFilter as kf
 from algorithms.window_slider import WindowSlider
 
-from sklearn.linear_model import LinearRegression
-
 from sklearn.preprocessing import StandardScaler
+import statsmodels.api as sm
+from scipy.stats import norm
+
 
 
 def matrix2np_arr(elements):
@@ -50,7 +51,11 @@ class KalmanFilterAlgorithm(BaseAlgorithm):
         
         slider = WindowSlider(file_name, int(self.params['window_size']), self.date_column, self.value_column)
         self.trends = []
-        self.intervals = []
+        self.obs_ci_lower, self.obs_ci_upper = [], []
+        self.mean_ci_lower, self.mean_ci_upper = [], []
+        linear_trends  = []
+        obs_ci_lower, obs_ci_upper = [], []
+        mean_ci_lower, mean_ci_upper = [], []
         
         for df in slider.slide():
             df[self.date_column] = pd.to_datetime(df[self.date_column])
@@ -58,32 +63,44 @@ class KalmanFilterAlgorithm(BaseAlgorithm):
             measurements = df[self.value_column]
             initial_price = measurements.iloc[0]
 
-            predictions = self.stock_predict(measurements, initial_price)
+            predictions = np.array(self.stock_predict(measurements, initial_price))
 
-            scaler = StandardScaler()
+            scaler = StandardScaler() # 
             date_index_datetime =  df.index.astype(np.int64) 
             x = date_index_datetime.to_numpy().reshape(-1, 1)
-            x_scaled = scaler.fit_transform(x)
-
-            model = LinearRegression()
-            model.fit(x_scaled, predictions)
+            x_scaled = sm.add_constant(scaler.fit_transform(x))
             
-            results = model.predict(x_scaled)
 
-            interval = 1.96 * np.std(predictions - results)
+            
 
-            linear_trends, intervals  = [], []
-            for i in range(0, len(results)):
-                trend_point = {'x': str(df.index[i]- pd.Timestamp("1970-01-01")) // pd.Timedelta('1ms'),
-                         'y': results[i]}
-                prediction_interval_point = {'x': str(df.index[i]- pd.Timestamp("1970-01-01")) // pd.Timedelta('1ms'),
-                         'y': interval}
+            
+            trend_point = {'x': str(df.index[len(predictions) - 1]- pd.Timestamp("1970-01-01")) // pd.Timedelta('1ms'),
+                        'y': predictions[len(predictions) - 1]}
+            
+            alpha = 0.1
+            cov_matrix = self.model.P  # Ковариационная матрица ошибок предсказания
+            std_errors = np.sqrt(np.diag(cov_matrix))  # Стандартные ошибки предсказания
+
+            mean_prediction = np.mean(predictions)
+            std_prediction = np.std(predictions)
+
+            mean_ci_lower_point = {'x': str(df.index[len(predictions) - 1]- pd.Timestamp("1970-01-01")) // pd.Timedelta('1ms'),
+                        'y': (mean_prediction - norm.ppf(1 - alpha/2) * std_prediction)}
+            mean_ci_upper_point = {'x': str(df.index[len(predictions) - 1]- pd.Timestamp("1970-01-01")) // pd.Timedelta('1ms'),
+                        'y': (mean_prediction + norm.ppf(1 - alpha/2) * std_prediction)}
+                        
                 
-                linear_trends.append(trend_point)
-                intervals.append(prediction_interval_point)
+            linear_trends.append(trend_point)
+            
+            mean_ci_lower.append(mean_ci_lower_point)
+            mean_ci_upper.append(mean_ci_upper_point)
+            
 
 
-            self.trends.append(linear_trends)
-            self.intervals.append(intervals)
-
-        return self.trends, self.intervals
+        self.trends.append(linear_trends)
+        self.mean_ci_lower.append(mean_ci_lower)
+        self.mean_ci_upper.append(mean_ci_upper)
+        self.obs_ci_lower.append(obs_ci_lower)
+        self.obs_ci_upper.append(obs_ci_upper)      
+            
+        return self.trends, self.obs_ci_lower, self.obs_ci_upper, self.mean_ci_lower, self.mean_ci_upper